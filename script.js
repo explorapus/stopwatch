251496866f78b24a44edec0ad130109df5db17c5
Squirrel <114249728+explorapus@users.noreply.github.com>
2024-04-23 16:16:31 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,119 @@
+document.body.onload = setup;
+let ets //elasped time (in) seconds
+let timeBefore, laps, going, lastLapTime
+
+function setup() {
+  lastLapTime = 0
+  ets = 0
+  laps = 1
+  going = false
+  starting = false
+  updateTime()
+  addElement(lastLapTime)
+}
+
+function updateTime() {
+  t = new Date()
+  if (typeof startTime == "undefined") {
+    document.getElementById("time").innerHTML = "00:00:00"
+  }
+  if (going) {
+    ets = Math.round((t - startTime) / 1000) + timeBefore
+    document.getElementById("time").innerHTML = formatTime(ets)
+    document.getElementById("info").firstChild.lastChild.innerHTML = formatTime(ets - lastLapTime)
+  } else {
+    timeBefore = ets
+  }
+}
+
+document.getElementById("start").addEventListener('click', function(e) {
+  if (starting == false) {
+    timeBefore = 0
+  }
+  starting = true
+  if (going == false) {
+    startTime = new Date()
+    going = true
+    document.getElementById("start").innerHTML = "pause"
+    document.getElementById("lap").innerHTML = "add"
+  } else {
+    going = false
+    document.getElementById("start").innerHTML = "play_arrow"
+    document.getElementById("lap").innerHTML = "restart_alt"
+  }
+})
+
+document.getElementById("lap").addEventListener('click', function(e) {
+  if (going) {
+    laps++
+    addElement(lastLapTime)
+    lastLapTime = ets
+  } else {
+    lastLapTime = 0
+    laps = 1
+    info.replaceChildren();
+    timeBefore = 0
+    ets = 0
+    addElement(lastLapTime)
+    document.getElementById("time").innerHTML = "00:00:00"
+  }
+})
+
+setInterval(updateTime, 100)
+
+function addElement(lastLapTime) {
+  const newDiv = document.createElement("div");
+
+  const lapNumber = document.createElement("div");
+  const lapNumberText = document.createTextNode(String(laps));
+  lapNumber.classList.add("lapCount");
+  lapNumber.appendChild(lapNumberText);
+  newDiv.insertAdjacentElement("afterbegin", lapNumber);
+
+  const lapName = document.createElement("div");
+  const lapNameText = document.createTextNode("Double click to replace");
+  lapName.classList.add("lapNameDiv");
+  lapName.setAttribute("id", "l" + String(laps));
+  lapName.appendChild(lapNameText);
+  newDiv.insertAdjacentElement("beforeend", lapName);
+
+  const lapTime = document.createElement("div");
+  const lapTimeText = document.createTextNode(formatTime(ets - lastLapTime));
+  lapTime.classList.add("lapTime");
+  lapTime.appendChild(lapTimeText);
+  newDiv.insertAdjacentElement("beforeend", lapTime);
+
+  newDiv.classList.add("lapDiv");
+  info.insertAdjacentElement("afterbegin", newDiv);
+}
+
+document.getElementById("info").addEventListener('dblclick', function(e) {
+  clicked = e.target
+  if (e.target.classList.contains('lapNameDiv')) {
+    //genius stuff from https://stackoverflow.com/questions/48977986/editing-form-by-double-clicking-element
+    var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    var fontSize = window.getComputedStyle(clicked).fontSize;
+    input.value = clicked.innerHTML
+    input.onblur = function() {
+      var val = this.value;
+      if (val.length < 1.5 * this.clientWidth / parseInt(fontSize)) {
+        this.parentNode.innerHTML = val;
+      }
+    }
+    clicked.innerHTML = "";
+    clicked.appendChild(input);
+    input.focus();
+  }
+})
+
+function formatTime(seconds) {
+  let ds, dm, dh // display second, minute, and hour
+  ds = String(seconds % 60)
+  ds = ds.length == 1 ? "0" + String(ds) : String(ds)
+  dm = String(Math.floor((seconds % 3600) / 60))
+  dm = dm.length == 1 ? "0" + String(dm) : String(dm)
+  dh = String(Math.floor(seconds / 3600))
+  dh = dh.length == 1 ? "0" + String(dh) : String(dh)
+  return dh + ":" + dm + ":" + ds
+}
